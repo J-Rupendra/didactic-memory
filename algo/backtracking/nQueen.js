609462f7ec46c6ac80ebe9a88c509abad7d7a33b
rupendraj <rupendraj21@gmail.com>
2025-02-17 18:15:11 +0530

    nqueen full code

--- algo/backtracking/nQueen.js
+++ algo/backtracking/nQueen.js
@@ -3,41 +3,51 @@
 // lets solve by considering placing queens in rows
 // in this problem if the pointer hits the base condition then we achieved the desired result
 
-function placeQueensInBoard(board, row, ans){
+function placeQueensInBoard(board, row){
     if(row === board.length){
-        console.log(ans,"asdf");
+        console.log(board.map(row=>row.join("")));
         return
     }
-    for(ind=0;ind<=row;ind++){
-        if(isSafe(board, row, ind)){
-            board[row][ind] = "Q"
-            ans.push(`(${row},${ind}),`)
-            placeQueensInBoard(board, row+1, ans)
-            ans.pop()
-            board[row][ind]="."
+    for(let col=0;col<board.length;col++){
+        if(isSafe(board, row, col)){
+            board[row][col] = "Q"
+            placeQueensInBoard(board, row+1)
+            board[row][col]="."
         }
+        
     }
 }
 
-function isSafe(board, row, ind){
-
-    let found = false
+function isSafe(board, row, col){
 
     // we are skipping row check as we put the queens row wise which means this row doesn't have any queen
 
 
     // column
-    for(let i=0; i<board.length;i++){
-        if(board[i][ind]==="Q"){
+    for(let i=0; i<row;i++){
+        if(board[i][col]==="Q"){
             return false
         }
     }
 
-    // diagonal
-    // need to implement
+    // upper left diagonal
+    for(let i=row-1,j=col-1; i>=0 && j>=0;i--,j--){
+        if(board[i][j]==="Q"){
+            return false
+        }
+    }
+    // upper right diagonal
+    for(let i=row-1,j=col+1; i>=0 && j<board.length;i++,j++){
+        if(board[i][j]==="Q"){
+            return false
+        }
+    }
+    return true
 
 }
 
-const chessBoard = Array.from( new Array(7), ele=> new Array(7).fill(".") )
+const chessBoard = Array.from( new Array(4), ele=> new Array(4).fill(".") )
 
 placeQueensInBoard(chessBoard,0,[])
+
+// time complexity = O(n!) => if we place a queen in a row then then next row has n-1 possibilities and then next row will have n-2 ....1
